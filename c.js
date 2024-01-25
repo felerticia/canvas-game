a2249b3e94d59bbdaf26f236ce127d7fc278b93d
Amirhossein Shahbabaee <amirhossein.shahbabaee@cleura.com>
2024-01-25 09:42:41 +0100

    Draw Hero

--- c.js
+++ c.js
@@ -6,9 +6,52 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 ctx.font = "bold 48px serif";
 
+class Hero {
+  constructor() {
+    this.image = new Image();
+    this.image.src = "./hero.png";
+    this.width = 200;
+    this.height = 156;
+    this.x = 50;
+    this.y = 100;
+    this.frame = 0;
+  }
+
+  show() {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    //ctx.drawImage(this.image, this.x, this.y);
+    //ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    // ctx.drawImage(
+    //   this.image,
+    //   0,
+    //   0,
+    //   this.width,
+    //   this.height,
+    //   this.x,
+    //   this.y,
+    //   this.width,
+    //   this.height
+    // );
+    ctx.drawImage(
+      this.image,
+      this.frame * this.width,
+      0,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+const hero = new Hero();
+
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillText("Text", 100, 100);
+  hero.show();
+  requestAnimationFrame(animate);
 }
 
 animate();
